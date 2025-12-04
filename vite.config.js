c07faec55b1c7f272b03352b2a10577d1d7c019c
Volodymyr Shkimba <shkimbavova@gmail.com>
2025-12-04 22:08:56 +0200

    fix delpoy bug

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import viteImagemin from "vite-plugin-imagemin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteImagemin({
-      pngquant: { quality: [0.6, 0.8] },
-      mozjpeg: { quality: 75 },
-    }),
-  ],
+  plugins: [react()],
 });
