5d44d18965385e8e05c58111d08e72453380239a
Volodymyr Shkimba <shkimbavova@gmail.com>
2025-12-05 16:36:51 +0200

    remove handler

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import clsx from "clsx";
 
 import Icon from "../Icon/Icon";
@@ -16,16 +16,6 @@ const Input = ({
   isSubmitSuccessful,
 }) => {
   const [visible, setVisible] = useState(false);
-  const inputRef = useRef(null);
-
-  const handleBlur = () => {
-    document.body.style.transform = "scale(0.999)";
-    document.body.style.transformOrigin = "top left";
-
-    setTimeout(() => {
-      document.body.style.transform = "scale(1)";
-    }, 50);
-  };
 
   return (
     <label
@@ -41,8 +31,6 @@ const Input = ({
         type={type === "password" && visible ? "text" : type}
         placeholder={placeholder}
         {...register(id)}
-        ref={inputRef}
-        onBlur={handleBlur}
       />
       {(error || (isSubmitted && !error && !isSubmitSuccessful)) && (
         <span
